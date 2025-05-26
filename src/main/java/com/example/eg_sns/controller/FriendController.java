5ca8c5e402ce6a486a47fce3a1650a8d212aedbe
小林温人 <ha.kobayashi@dcworks.jp>
2025-05-26 14:15:58 +0900

    フレンドリスト機能のテンプレートをコピー

---
+++ src/main/java/com/example/eg_sns/controller/FriendController.java
@@ -0,0 +1,18 @@
+package com.example.eg_sns.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ※TODO 適宜実装を入れてください。
+ */
+@Controller
+@RequestMapping("/friend")
+public class FriendController {
+
+	@GetMapping("/list")
+	public String list() {
+		return "friend/list";
+	}
+}
