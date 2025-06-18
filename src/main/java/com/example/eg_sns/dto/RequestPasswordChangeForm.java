03c369a8c0bf05f3e5c610ba25b86531fef22fca
小林温人 <ha.kobayashi@dcworks.jp>
2025-06-18 13:14:39 +0900

    パスワード変更のバリデーションを追加

--- src/main/java/com/example/eg_sns/dto/RequestPasswordChangeForm.java
+++ src/main/java/com/example/eg_sns/dto/RequestPasswordChangeForm.java
@@ -1,5 +1,8 @@
 package com.example.eg_sns.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,11 +11,17 @@ import lombok.EqualsAndHashCode;
 public class RequestPasswordChangeForm extends DtoBase {
 
 	/** 現在のパスワード */
+	@NotBlank(message = "現在のパスワードを入力してください。")
+	@Size(max = 32, message = "パスワードは最大32文字です。")
 	private String currentPassword;
 
 	/** 新しいパスワード */
+	@NotBlank(message = "新しいパスワードを入力してください。")
+	@Size(max = 32, message = "パスワードは最大32文字です。")
 	private String newPassword;
 
 	/** 新しいパスワード（再入力） */
+	@NotBlank(message = "新しいパスワード（再入力）を入力してください。")
+	@Size(max = 32, message = "パスワードは最大32文字です。")
 	private String confirmPassword;
 }
